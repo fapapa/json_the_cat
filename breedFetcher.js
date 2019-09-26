32259421495fa0e216c5a2307176b52303a045c8
Fabio Papa <fabtheman@gmail.com>
2019-09-26 13:46:28 -0700

    Refactor: Move request into a function that takes a query and a CB

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,30 @@
 const request = require('request');
 const API_URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const query = process.argv[2];
+const fetchBreedDescription = (query, complete) => {
+  request(API_URL + query, (err, response, body) => {
+    if (err) {
+      complete(err, null);
+      return;
+    }
 
-request(API_URL + query, (err, response, body) => {
-  if (err) {
-    console.log('ERROR! Request failed:');
-    console.log(err);
-    return;
-  }
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      complete(null, data[0].description);
+    } else {
+      complete(new Error(`No results found`), null);
+    }
+  });
+};
 
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    console.log(data[0].description);
+fetchBreedDescription(process.argv[2], (err, description) => {
+  if (err && err.code === 'ENOTFOUND') {
+    console.error('ERROR! Request failed:');
+    console.error(err);
+  } else if (err && err.message === 'No results found') {
+    console.error('ERROR! No results found:');
+    console.error('No results were returned for the query.');
   } else {
-    console.log(`Search for "${query}" returned no results.`);
+    console.log(description);
   }
 });
