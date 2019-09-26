846870420a50434a2850ac2579acc8e52cfddfbc
Fabio Papa <fabtheman@gmail.com>
2019-09-26 12:40:10 -0700

    Do a simple search on the API for breed, and log the description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,9 @@
 const request = require('request');
+const API_URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+const query = process.argv[2];
+
+request(API_URL + query, (err, response, body) => {
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+});
