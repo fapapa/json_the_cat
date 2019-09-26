4ee2103feca861f72cd4a00adf3060ffbab36cb3
Fabio Papa <fabtheman@gmail.com>
2019-09-26 12:49:13 -0700

    Log an error in case request is not successful

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,6 +4,12 @@ const API_URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
 const query = process.argv[2];
 
 request(API_URL + query, (err, response, body) => {
+  if (err) {
+    console.log('ERROR! Request failed:');
+    console.log(err);
+    return;
+  }
+
   const data = JSON.parse(body);
   if (data.length > 0) {
     console.log(data[0].description);
